8af274c998dc28c1e64153b1369727118c302cf8
dodoburner <100496179+dodoburner@users.noreply.github.com>
2023-03-31 16:43:09 +0200

    Update api link

--- src/redux/utils.js
+++ src/redux/utils.js
@@ -1,5 +1,6 @@
 import { toast } from "react-toastify";
 
-export const API_URL = 'https://microverse-final-capstone-backend.onrender.com/';
+// export const API_URL = 'http://localhost:3000';
+export const API_URL = 'https://rails-wq0k.onrender.com';
 export const API_KEY = "";
 export const handleToast = ({ msg, type = "success" }) => toast(msg, { position: "top-center", type, autoClose: 5000 });
