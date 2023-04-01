b0d50968f95c97aa0a10261c28882e5c23b3ab0a
dodoburner <100496179+dodoburner@users.noreply.github.com>
2023-04-01 16:30:04 +0200

    Update api link

--- src/redux/utils.js
+++ src/redux/utils.js
@@ -1,6 +1,6 @@
 import { toast } from "react-toastify";
 
 // export const API_URL = 'http://localhost:3000';
-export const API_URL = 'https://rails-wq0k.onrender.com';
+export const API_URL = 'https://rails-zx7a.onrender.com';
 export const API_KEY = "";
 export const handleToast = ({ msg, type = "success" }) => toast(msg, { position: "top-center", type, autoClose: 5000 });
